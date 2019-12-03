49acb09ec2037c76eecf6b8cf6c9ea5fd39bf1d3
MICHELEFREITAS <michele-braga@hotmail.com>
2019-12-03 13:27:10 +0000

    Projeto iniciado

---
+++ src/test/java/com/example/michelefreitas/workshopmongo/ProjetoWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.michelefreitas.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
